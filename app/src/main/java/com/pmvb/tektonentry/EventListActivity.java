b9e2778bc1175620d9c89565063614afaf26acd2
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-02 23:14:50 -0500

    Add basic login flow to EventListActivity

--- app/src/main/java/com/pmvb/tektonentry/EventListActivity.java
+++ app/src/main/java/com/pmvb/tektonentry/EventListActivity.java
@@ -2,6 +2,7 @@ package com.pmvb.tektonentry;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -9,15 +10,17 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-
+import com.crashlytics.android.Crashlytics;
 import com.pmvb.tektonentry.dummy.DummyContent;
-
 import java.util.List;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * An activity representing a list of Events. This activity
@@ -28,6 +31,7 @@ import java.util.List;
  * item details side-by-side using two vertical panes.
  */
 public class EventListActivity extends AppCompatActivity {
+    public static final String TAG = "EventListActivity";
 
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
@@ -38,6 +42,20 @@ public class EventListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+
+        Log.d(TAG, "Before loading prefs");
+        SharedPreferences prefs = getSharedPreferences(
+                getString(R.string.prefs_file_key), MODE_PRIVATE);
+        Log.d(TAG, "Loaded prefs");
+        if (!prefs.getBoolean(getString(R.string.loggedIn_pref_key), false)) {
+            Log.d(TAG, "User is not logged in. Redirect to login");
+            Intent login = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(login);
+            finish();
+        }
+        Log.d(TAG, "User is logged in. Load event list");
+
         setContentView(R.layout.activity_event_list);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -62,6 +80,33 @@ public class EventListActivity extends AppCompatActivity {
             // activity should be in two-pane mode.
             mTwoPane = true;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Add items to the action bar if present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item click
+        int id = item.getItemId();
+
+        if (id == R.id.menu_action_logout) {
+            logoutUser();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logoutUser() {
+        // Launch LoginActivity with logout flag
+        Intent logout = new Intent(this, LoginActivity.class);
+        logout.putExtra("logout", true);
+        startActivity(logout);
+        finish();
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
