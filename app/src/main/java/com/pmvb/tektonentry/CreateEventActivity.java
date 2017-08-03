df3dcb0ee73ca9d68260d5287d81482ca75ff4ae
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-03 18:31:59 -0500

    Add basic toolbar setup in CreateEventActivity

--- app/src/main/java/com/pmvb/tektonentry/CreateEventActivity.java
+++ app/src/main/java/com/pmvb/tektonentry/CreateEventActivity.java
@@ -1,7 +1,9 @@
 package com.pmvb.tektonentry;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class CreateEventActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class CreateEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.event_edit_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.create_event_title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
