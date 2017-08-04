7072ee73e3b858e7fe4c79d09e291ce362420d5d
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-03 23:11:03 -0500

    Add basic DatePickerDialog setup

--- app/src/main/java/com/pmvb/tektonentry/CreateEventActivity.java
+++ app/src/main/java/com/pmvb/tektonentry/CreateEventActivity.java
@@ -1,21 +1,69 @@
 package com.pmvb.tektonentry;
 
+import android.app.DatePickerDialog;
+
+import java.util.Calendar;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CreateEventActivity extends AppCompatActivity {
+
+    @BindView(R.id.event_input_name)
+    TextInputEditText nameField;
+    @BindView(R.id.event_input_date)
+    TextInputEditText dateField;
+    @BindView(R.id.event_input_time)
+    TextInputEditText timeField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+        ButterKnife.bind(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.event_edit_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.create_event_title);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setupDateField();
+    }
+
+    private void setupDateField() {
+        dateField.setOnFocusChangeListener((view, hasFocus) -> {
+            if (hasFocus) {
+                // Current date
+                int thisYear;
+                int thisMonth;
+                int thisDay;
+                if (dateField.getText().toString().isEmpty()) {
+                    Calendar c = Calendar.getInstance();
+                    thisYear = c.get(Calendar.YEAR);
+                    thisMonth = c.get(Calendar.MONTH);
+                    thisDay = c.get(Calendar.DAY_OF_MONTH);
+                } else {
+                    String[] tokens = dateField.getText().toString().split("-");
+                    thisYear = Integer.parseInt(tokens[0]);
+                    thisMonth = Integer.parseInt(tokens[1]) - 1;
+                    thisDay = Integer.parseInt(tokens[2]);
+                }
+                DatePickerDialog pickerDialog = new DatePickerDialog(
+                        this,
+                        (picker, year, month, dayOfMonth) -> {
+                            dateField.setText(String.format("%d-%02d-%02d", year, month + 1, dayOfMonth));
+                            nameField.requestFocus();
+                        },
+                        thisYear, thisMonth, thisDay
+                );
+                pickerDialog.show();
+            }
+        });
     }
 
     @Override
@@ -23,4 +71,6 @@ public class CreateEventActivity extends AppCompatActivity {
         onBackPressed();
         return true;
     }
+
+
 }
