6e160ba9fa7bde55437b35f39615c7cfccfdf1fd
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-04 14:21:19 -0500

    Add Event and EventParcelable classes

---
+++ app/src/main/java/com/pmvb/tektonentry/event/Event.java
@@ -0,0 +1,84 @@
+package com.pmvb.tektonentry.event;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by pmvb on 17-08-04.
+ */
+
+class Event {
+    private String name;
+    private Calendar date;
+    private int hourOfDay;
+    private int minute;
+    private LatLng location;
+
+    public Event(String name, Date date, int hourOfDay, int minute, LatLng location) {
+        this.setName(name);
+        this.setDate(Calendar.getInstance());
+        this.getDate().setTime(date);
+        this.setHourOfDay(hourOfDay);
+        this.setMinute(minute);
+        this.setLocation(location);
+    }
+
+    public Event(String name, Date date, int hourOfDay, int minute, double latitude, double longitude) {
+        this(name, date, hourOfDay, minute, new LatLng(latitude, longitude));
+    }
+
+    public Event(String name, Calendar date, int hourOfDay, int minute, LatLng location) {
+        this(name, date.getTime(), hourOfDay, minute, location);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public int getHourOfDay() {
+        return hourOfDay;
+    }
+
+    public void setHourOfDay(int hourOfDay) {
+        this.hourOfDay = hourOfDay;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public LatLng getLocation() {
+        return location;
+    }
+
+    public void setLocation(LatLng location) {
+        this.location = location;
+    }
+
+    public String getDateStr() {
+        return String.format(
+                "%d-%02d-%02d",
+                date.get(Calendar.YEAR),
+                date.get(Calendar.MONTH) + 1,
+                date.get(Calendar.DAY_OF_MONTH)
+        );
+    }
+}
---
+++ app/src/main/java/com/pmvb/tektonentry/event/EventParcelable.java
@@ -0,0 +1,81 @@
+package com.pmvb.tektonentry.event;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Calendar;
+
+/**
+ * Created by pmvb on 17-08-03.
+ */
+
+public class EventParcelable implements Parcelable{
+    private Event event;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(getEvent().getName());
+        parcel.writeIntArray(new int[] {
+                getEvent().getDate().get(Calendar.YEAR),
+                getEvent().getDate().get(Calendar.MONTH) + 1,
+                getEvent().getDate().get(Calendar.DAY_OF_MONTH)
+        });
+//        parcel.writeString(String.format("%02d:%02d", hourOfDay, minute));
+        parcel.writeIntArray(new int[] {
+                getEvent().getHourOfDay(),
+                getEvent().getMinute()
+        });
+        parcel.writeDouble(getEvent().getLocation().latitude);
+        parcel.writeDouble(getEvent().getLocation().longitude);
+    }
+
+    public static final Parcelable.Creator<EventParcelable> CREATOR = new Parcelable.Creator<EventParcelable>() {
+        public EventParcelable createFromParcel(Parcel parcel) {
+            return new EventParcelable(parcel);
+        }
+
+        @Override
+        public EventParcelable[] newArray(int i) {
+            return new EventParcelable[i];
+        }
+    };
+
+    private EventParcelable(Parcel parcel) {
+        String name = parcel.readString();
+
+        int[] dateValues = new int[3];
+        parcel.readIntArray(dateValues);
+        Calendar date = Calendar.getInstance();
+        date.set(Calendar.YEAR, dateValues[0]);
+        date.set(Calendar.MONTH, dateValues[1] - 1);
+        date.set(Calendar.DAY_OF_MONTH, dateValues[2]);
+
+        int[] timeValues = new int[2];
+        parcel.readIntArray(timeValues);
+        int hourOfDay = timeValues[0];
+        int minute = timeValues[1];
+
+        LatLng location = new LatLng(parcel.readDouble(), parcel.readDouble());
+
+        setEvent(new Event(name, date, hourOfDay, minute, location));
+    }
+
+    public EventParcelable(Event evt) {
+        setEvent(evt);
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+}
