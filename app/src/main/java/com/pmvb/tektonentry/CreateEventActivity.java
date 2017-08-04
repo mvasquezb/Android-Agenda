4d4d58aa12a3326db95d56d651b7e52061f660a1
Martín Vásquez <pedromvasquezb@gmail.com>
2017-08-03 23:32:22 -0500

    Add TimePickerDialog basic setup in CreateEventActivity

--- app/src/main/java/com/pmvb/tektonentry/CreateEventActivity.java
+++ app/src/main/java/com/pmvb/tektonentry/CreateEventActivity.java
@@ -3,11 +3,15 @@ package com.pmvb.tektonentry;
 import android.app.DatePickerDialog;
 
 import java.util.Calendar;
+
+import android.app.IntentService;
+import android.app.TimePickerDialog;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TimePicker;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,6 +37,36 @@ public class CreateEventActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         setupDateField();
+        setupTimeField();
+    }
+
+    private void setupTimeField() {
+        timeField.setOnFocusChangeListener((view, hasFocus) -> {
+            if (hasFocus) {
+                int selHour;
+                int selMinute;
+                boolean is24Hour = true;
+                String timeStr = timeField.getText().toString();
+                if (timeStr.isEmpty()) {
+                    final Calendar c = Calendar.getInstance();
+                    selHour = c.get(Calendar.HOUR_OF_DAY);
+                    selMinute = c.get(Calendar.MINUTE);
+                } else {
+                    String[] tokens = timeStr.split(":");
+                    selHour = Integer.parseInt(tokens[0]);
+                    selMinute = Integer.parseInt(tokens[1]);
+                }
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        this,
+                        (timePicker, hour, minute) -> {
+                            timeField.setText(String.format("%02d:%02d", hour, minute));
+                            timeField.clearFocus();
+                        },
+                        selHour, selMinute, is24Hour
+                );
+                timePickerDialog.show();
+            }
+        });
     }
 
     private void setupDateField() {
@@ -42,26 +76,27 @@ public class CreateEventActivity extends AppCompatActivity {
                 int thisYear;
                 int thisMonth;
                 int thisDay;
-                if (dateField.getText().toString().isEmpty()) {
+                String dateStr = dateField.getText().toString();
+                if (dateStr.isEmpty()) {
                     Calendar c = Calendar.getInstance();
                     thisYear = c.get(Calendar.YEAR);
                     thisMonth = c.get(Calendar.MONTH);
                     thisDay = c.get(Calendar.DAY_OF_MONTH);
                 } else {
-                    String[] tokens = dateField.getText().toString().split("-");
+                    String[] tokens = dateStr.split("-");
                     thisYear = Integer.parseInt(tokens[0]);
                     thisMonth = Integer.parseInt(tokens[1]) - 1;
                     thisDay = Integer.parseInt(tokens[2]);
                 }
-                DatePickerDialog pickerDialog = new DatePickerDialog(
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
                         this,
                         (picker, year, month, dayOfMonth) -> {
                             dateField.setText(String.format("%d-%02d-%02d", year, month + 1, dayOfMonth));
-                            nameField.requestFocus();
+                            dateField.clearFocus();
                         },
                         thisYear, thisMonth, thisDay
                 );
-                pickerDialog.show();
+                datePickerDialog.show();
             }
         });
     }
